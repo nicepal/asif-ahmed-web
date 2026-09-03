8fd9f1ab66b6ea8493bf5690900e3378f0fe80fa
Asif Ahmed <nicepal@MacBook-Pro.local>
2026-09-04 00:27:52 +0500

    added exo

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "asifahmed",
+      cwd: __dirname,
+      script: "npm",
+      args: "start",
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: "10s",
+      env: {
+        NODE_ENV: "production",
+        PORT: "8500",
+        NEXT_PUBLIC_SITE_URL: "https://asifahmed.tech",
+        VINEXT_TRUST_PROXY: "1",
+        VINEXT_TRUSTED_HOSTS: "asifahmed.tech,www.asifahmed.tech",
+        WRANGLER_LOG_PATH: ".wrangler/wrangler.log",
+      },
+    },
+  ],
+};
